2ac8468722292220b950724b9169c531b1503c8a
prawdziwysar <131494252+prawdziwysar@users.noreply.github.com>
2023-04-22 20:01:46 +0200

    add words

--- src/app/routes/main/react/words/presenters/WordsPresenter.ts
+++ src/app/routes/main/react/words/presenters/WordsPresenter.ts
@@ -1,12 +1,51 @@
-// eslint-disable import/no-unresolved
-import all from 'stopwords-json'
+import all from './pl.json'
 
 export default function WordsPresenter({ input }: { input: string }) {
     return {
         getWords() {
-            console.log(all)
+            const possibleLetters = input
+                .replaceAll(' ', '')
+                .toLowerCase()
+                .split('')
+                .reduce((current, newone) => {
+                    const element = current.find(
+                        (value) => value.letter === newone
+                    )
 
-            return all.pl as string[]
+                    if (element) {
+                        element.counter += 1
+
+                        return current
+                    }
+
+                    return [...current, { letter: newone, counter: 1 }]
+                }, [] as { letter: string; counter: number }[])
+
+            return all
+                .filter((word) => {
+                    return word
+                        .split('')
+                        .reduce((current, newone) => {
+                            const element = current.find(
+                                (value) => value.letter === newone
+                            )
+
+                            if (element) {
+                                element.counter += 1
+
+                                return current
+                            }
+
+                            return [...current, { letter: newone, counter: 1 }]
+                        }, [] as { letter: string; counter: number }[])
+                        .every(
+                            ({ letter, counter }) =>
+                                (possibleLetters.find(
+                                    (e) => e.letter === letter
+                                )?.counter ?? 0) >= counter
+                        )
+                })
+                .sort((a, b) => b.length - a.length)
         },
     }
 }
